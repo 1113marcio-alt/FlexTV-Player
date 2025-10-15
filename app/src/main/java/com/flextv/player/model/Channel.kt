bc82b3cf7a9f28d68bed15c389f3353c37efda38
1113marcio-alt <1113marcio@gmail.com>
2025-10-15 11:16:14 +0200

    Update Channel.kt

--- app/src/main/java/com/flextv/player/model/Channel.kt
+++ app/src/main/java/com/flextv/player/model/Channel.kt
@@ -1,1 +1,18 @@
+package com.flextv.player.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Classe de modelo que representa um canal.
+ * Também é uma entidade para o banco de dados Room.
+ */
+@Entity(tableName = "channels")
+data class Channel(
+    @PrimaryKey val id: String, // Geralmente extraído de tvg-id do M3U
+    val name: String,
+    val group: String,
+    val logoUrl: String?,
+    val url: String,
+    var isFavorite: Boolean = false
+)
